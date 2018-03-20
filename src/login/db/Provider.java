03b0a70d9e8d7c75f68c3521186b4ec505d1dbee
uks007 <21065217+uks1207@users.noreply.github.com>
2018-03-20 13:04:33 +0530

    updated provider

--- src/login/db/Provider.java
+++ src/login/db/Provider.java
@@ -1,5 +1,25 @@
 package login.db;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class Provider {
 
+	public static Connection con=null;
+	public static Connection getConnection()
+	{
+		if(con==null)
+		{
+			try
+			{
+				Class.forName("oracle.jdbc.OracleDriver");
+				con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return con;
+		
+		
+	}
 }
